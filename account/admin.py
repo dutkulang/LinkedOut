11d705b4b07ad0f695866ca73ad6fe082944178e
Dut <dutandrew78@gmail.com>
2023-12-30 15:24:52 +0300

    add account app

---
+++ account/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Profile, Language, Skill, Country, Hub
+
+admin.site.register(Profile)
+admin.site.register(Language)
+admin.site.register(Skill)
+admin.site.register(Country)
+admin.site.register(Hub)
